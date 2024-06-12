718868b47f0e4e1c6f3b904e38c4e49df81e4c59
ndyabagye <ndyabagyehenrytusi@gmail.com>
2024-06-12 10:33:51 +0300

    Generate token on the server and use it to authorize client to get resource

---
+++ README.md
@@ -0,0 +1,4 @@
+### BEARER TOKEN AUTHENTICATION
+
+Using bearer tokens generated on the server to allow clients
+access resources.
--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+var tokens []string
+
 func main() {
 	r := gin.Default()
-	r.GET("/resource", gin.BasicAuth(gin.Accounts{
+	r.POST("/login", gin.BasicAuth(gin.Accounts{
 		"admin": "secret",
 	}), func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"data": "resource data",
-		})
+		token, _ := randomHex(20)
+		tokens = append(tokens, token)
+
+		c.JSON(http.StatusOK, gin.H{"token": token})
+	})
+
+	r.GET("/resource", func(c *gin.Context) {
+		bearerToken := c.Request.Header.Get("Authorization")
+		reqToken := strings.Split(bearerToken, " ")[1]
+		for _, token := range tokens {
+			if token == reqToken {
+				c.JSON(http.StatusOK, gin.H{"data": "resource data"})
+				return
+			}
+		}
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "unauthorized"})
 	})
 
 	r.Run() // Listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func randomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
 }
